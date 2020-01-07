9b238f755a7d579de6c75ec3117a320513d4e184
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2020-01-06 23:11:53 -0300

    first commit, doing layout component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `henriquers Blog`,
+    description: `A blog about software development and other stuffs.`,
+    author: `@henriquerochars`,
+    position: `Full-Stack Developer`,
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
