1efd646845b546b1cd65eb91beab2b20eb59552f
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2020-01-07 23:36:04 -0300

    create Avatar, Profile, Sidebar and SocialLinks components

---
+++ src/components/SocialLinks/content.js
@@ -0,0 +1,22 @@
+const handle = "henriquerochars"
+
+const links = [
+  {
+    label: "LinkedinSquare",
+    url: "https://www.linkedin.com/in/henrique-rocha-serrano-73b81813b/",
+  },
+  {
+    label: "Github",
+    url: `https://github.com/${handle}`,
+  },
+  {
+    label: "Instagram",
+    url: `https://www.instagram.com/${handle}`,
+  },
+  {
+    label: "Twitter",
+    url: `https://twitter.com/${handle}`,
+  },
+]
+
+export default links
