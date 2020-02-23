ee709b1c427761122dbf55c52892b7740ae4ea5a
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2020-02-23 17:35:24 -0300

    feat(getthemecolor utils): create getThemeColor function util

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -4,10 +4,12 @@ import { useStaticQuery, graphql } from "gatsby"
 import Avatar from "../Avatar"
 import * as S from "./styled"
 
+import getThemeColor from "../../utils/getThemeColor"
+
 const Profile = () => {
   const {
     site: {
-      siteMetadata: { title, position, description, aboutMe },
+      siteMetadata: { title, position, description },
     },
   } = useStaticQuery(graphql`
     query MySiteMetadata {
@@ -24,7 +26,13 @@ const Profile = () => {
 
   return (
     <S.ProfileWrapper>
-      <S.ProfileLink cover direction="left" bg="#16202c" duration={0.6} to="/">
+      <S.ProfileLink
+        cover
+        direction="left"
+        bg={getThemeColor()}
+        duration={0.6}
+        to="/"
+      >
         <Avatar />
         <S.ProfileAuthor>
           {title}
