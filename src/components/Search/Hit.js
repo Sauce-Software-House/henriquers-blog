b4be2ded05d3f32579696049ee54da0121b9fe3b
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2020-02-29 22:55:03 -0300

    fix: background color in hit component on search page

--- src/components/Search/Hit.js
+++ src/components/Search/Hit.js
@@ -4,7 +4,7 @@ import PostItem from "../PostItem"
 const Hit = ({ hit }) => (
   <PostItem
     slug={hit.fields.slug}
-    backgrounbd={hit.background}
+    background={hit.background}
     title={hit.title}
     date={hit.date}
     description={hit.description}
