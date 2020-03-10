c457f060a8dbbf2562bee243bedff3ad1e920752
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2020-03-09 21:19:53 -0300

    fix(remove algolia search): remove algolia search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,22 +1,22 @@
 import React from "react"
 
-import algoliasearch from "algoliasearch/lite"
-import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
+// import algoliasearch from "algoliasearch/lite"
+// import { InstantSearch, SearchBox, Hits, Stats } from "react-instantsearch-dom"
 
-import Hit from "./Hit"
+// import Hit from "./Hit"
 import * as S from "./styled"
 
-const algolia = {
-  appId: process.env.GATSBY_ALGOLIA_APP_ID,
-  srarchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
-  indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-}
+// const algolia = {
+//   appId: process.env.GATSBY_ALGOLIA_APP_ID,
+//   srarchOnlyApiKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
+//   indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+// }
 
-const searchClient = algoliasearch(algolia.appId, algolia.srarchOnlyApiKey)
+// const searchClient = algoliasearch(algolia.appId, algolia.srarchOnlyApiKey)
 
 const Search = () => (
   <S.SearchWrapper>
-    <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
+    {/* <InstantSearch searchClient={searchClient} indexName={algolia.indexName}>
       <SearchBox translations={{ placeholder: "Pesquisar..." }} />
       <Stats
         translations={{
@@ -26,7 +26,7 @@ const Search = () => (
         }}
       />
       <Hits hitComponent={Hit} />
-    </InstantSearch>
+    </InstantSearch> */}
   </S.SearchWrapper>
 )
 
